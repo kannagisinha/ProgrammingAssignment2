d8eb768b59c5ac8853cfe9af36cc4d321421a4b1
Kannagi <kannagisinha1999@gmail.com>
2020-04-04 19:35:08 +0530

    hi

--- cachematrix.R
+++ cachematrix.R
@@ -6,40 +6,40 @@
 ##get - returns the matrix, setInverse - method to set inverse of matrix
 ##getInverse - method to get inverse of matrix 
 makeCacheMatrix <- function(q = matrix())
- {
-
-    ## Initialize the inverse property
-    p <- NULL
-
-    ## Method to set the matrix
-    set <- function( matrix ) 
 {
-            q <<- matrix
-            p <<- NULL
-}
-
-
-    get <- function() 
-{
-    	## Return the matrix
-    	q
-}
-    setInverse <- function(inverse) 
-{
-        p <<- inverse
- }
-
   
-    getInverse <- function() 
-{
-      
-        p
-    }
-
-    ## Return a list of the methods
-    return(list(set = set, get = get,
-         setInverse = setInverse,
-         getInverse = getInverse))
+  ## Initialize the inverse property
+  p <- NULL
+  
+  ## Method to set the matrix
+  set <- function( matrix ) 
+  {
+    q <<- matrix
+    p <<- NULL
+  }
+  
+  
+  get <- function() 
+  {
+    ## Return the matrix
+    q
+  }
+  setInverse <- function(inverse) 
+  {
+    p <<- inverse
+  }
+  
+  
+  getInverse <- function() 
+  {
+    
+    p
+  }
+  
+  ## Return a list of the methods
+  return(list(set = set, get = get,
+              setInverse = setInverse,
+              getInverse = getInverse))
 }
 
 
@@ -47,28 +47,28 @@ makeCacheMatrix <- function(q = matrix())
 ## above. If the inverse has already been calculated (and the matrix has not
 ## changed), then the "cachesolve" should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-
-    ## Return a matrix that is the inverse of 'x'
-    inv_matrix <- x$getInverse()
-
-    ## Just return the inverse if its already set
-    if( !is.null(inv_matrix) ) {
-            message("getting cached data")
-            return(inv_matrix)
-    }
-   else
-    {
+  
+  ## Return a matrix that is the inverse of 'x'
+  inv_matrix <- x$getInverse()
+  
+  ## Just return the inverse if its already set
+  if( !is.null(inv_matrix) ) {
+    message("getting cached data")
+    return(inv_matrix)
+  }
+  else
+  {
     ## Get the matrix from our object
     d <- x$get()
-
+    
     ## Calculate the inverse using matrix multiplication
     inv_matrix<- solve(d) %*% d
-
+    
     ## Set the inverse to the object
     x$setInverse(inv_matrix)
-
+    
     ## Return the matrix
     inv_matrix
-    }
+  }
 }
 
